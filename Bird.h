632a1de6fb6332a35f072431e82fbb13d3ee2bb4
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:14:39 +0200

    Bird class

--- Bird.h
+++ Bird.h
@@ -4,7 +4,29 @@
 #include "Animal.h"
 
 //define class bird here 
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+class Bird : public Animal {
+private:
+    float wingSpan;
+public:
+    Bird();
+    Bird(string n , int a ,float s , bool isH) : Animal(n,a,isH) {
+        wingSpan = s;
+    };
+    ~Bird();
 
+    float getSpan();
+    void setSpan();
+
+
+};
+
+#endif //ASSIGNMENT01_BIRD_H
 
 
 

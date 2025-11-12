5183f4c74762b2b25007d47d6773070ad6f75d49
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:17:06 +0200

    visitor class
    Added Visitor class with member variables and methods.

--- Visitor.h
+++ Visitor.h
@@ -5,7 +5,28 @@
 using namespace std;
 
 //define visitor class here
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+using namespace std;
+class Visitor {
+private:
+    string visitorName;
+    int ticketsabought;
+public:
+    Visitor();
+    Visitor(string n , int t);
+    ~Visitor();
+    void displayInfo();
 
+    void getName();
+    void getticketNum();
+
+    string setName();
+    int ticketNum();
+};
+
+#endif //ASSIGNMENT01_VISITOR_H
 
 
 

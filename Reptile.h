2d9ff30bb64da5e06e824d08f33ee98b826b84b0
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:16:33 +0200

    Reptile class

--- Reptile.h
+++ Reptile.h
@@ -4,7 +4,24 @@
 #include "Animal.h"
 
 //define reptile class here
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+class Reptile : public Animal{
+private:
+    bool isVenomous;
+public:
+    Reptile();
+    Reptile(string n , int a , bool is , bool isH) : Animal(n,a , isH){};
+    ~Reptile();
 
+    bool GetVen();
+};
+
+#endif //ASSIGNMENT01_REPTILE_H
 
 
 

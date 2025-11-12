12ff05d9de761329fe4205ea1e6a765e86e6ecf5
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:14:10 +0200

    Animal class
    Added Animal class with constructors, destructor, and member functions.

--- Animal.h
+++ Animal.h
@@ -5,7 +5,29 @@
 using namespace std;
 
 //define animal class here 
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
+public:
+    Animal();
+    Animal(string n , int a , bool isH);
+    ~Animal();
+    void display();
+    void feed();
 
+    string getName();
+    int getAge();
+
+    void setName();
+    void setAge();
+};
+
+#endif //ASSIGNMENT01_ANIMAL_H
 
 
 #endif

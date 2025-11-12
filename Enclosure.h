fbb79877b6f78fd930b44a8b8d0cebd69254c7f3
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:17:48 +0200

    Enclosure class

--- Enclosure.h
+++ Enclosure.h
@@ -4,7 +4,26 @@
 #include "Animal.h"
 
 //define enclosure class here 
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+#include <string>
+using namespace std;
+class Enclosure : public Animal{
+private:
+    Animal* animals;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure();
+    Enclosure( string name , int age , int cap , int curr , Animal* a) : Animal(name,age){};
+    ~Enclosure();
 
+    void addAnimal(Animal * a);
+    void displayAnimals();
+};
+
+#endif //ASSIGNMENT01_ENCLOSURE_H
 
 
 

b4b02932364be93d6fb07fa893491d03cc46c9df
Abdelrahman ElGabarty16008929 <abdelrahman.yousry@student.giu-uni.de>
2025-11-12 15:16:05 +0200

    Mammal class

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,30 @@
 #include "Animal.h"
 
 //define mammal class here
+//
+// Created by COMPUMARTS on 11/12/2025.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+class Mammal : public Animal {
+private:
+    string furColor;
+public:
+    Mammal();
+    Mammal(string c , string name , int age , bool isH) : Animal(name,age,isH) {
+        furColor = c;
+    }
 
+    ~Mammal();
+
+    string getfurColor();
+    void setFurColor()const;
+
+};
+
+
+#endif //ASSIGNMENT01_MAMMAL_H
 
 
 
